2662bb08881b0921ea73a4516b141a8eefc8b03a
Anne-Okingo <annekadiso@gmail.com>
2025-07-03 18:02:54 +0300

    feat: get login form and add an even lister associated with its submission

--- script/auth.js
+++ script/auth.js
@@ -1,28 +1,6 @@
-document.getElementById("loginForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
-  
-    const identifier = document.getElementById("identifier").value;
-    const password = document.getElementById("password").value;
-    const credentials = btoa(`${identifier}:${password}`);
-  
-    try {
-      const res = await fetch("https://learn.zone01kisumu.ke/api/auth/signin", {
-        method: "POST",
-        headers: { Authorization: `Basic ${credentials}` },
-      });
-  
-      if (!res.ok) throw new Error("Invalid credentials");
-  
-      const jwt = await res.text();
-      localStorage.setItem("jwt", jwt);
-      window.location.href = "profile.html";
-    } catch (err) {
-      document.getElementById("error").innerText = err.message;
-    }
-  });
-  
-  function logout() {
-    localStorage.removeItem("jwt");
-    window.location.href = "login.html";
-  }
-  
+//get the login form from the dom element
+document.getElementById('loginForm').addEventListener('submit', async function (e) {
+
+  e.preventDefault();
+
+})
