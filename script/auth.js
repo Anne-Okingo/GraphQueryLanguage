86009422249bf975c08425868443420d06a5cbf7
Anne-Okingo <annekadiso@gmail.com>
2025-07-03 18:28:42 +0300

    feat: add get credetials from the user input from the login form

--- script/auth.js
+++ script/auth.js
@@ -2,5 +2,10 @@
 document.getElementById('loginForm').addEventListener('submit', async function (e) {
 
   e.preventDefault();
+  
+const identifider = document.getElementById('identifier').value;
+const password = document.getElementById('password').value;
 
+const errorElem = document.getElementById('error');
+errorElem.textContent = '';
 })
